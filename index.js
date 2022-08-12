1b4ba85f09303ab4c3eb93b7f1af2ce6b6492136
Vitor Amilcar <vitor.amilcar@gmail.com>
2022-08-12 00:59:18 -0300

    att node js folder Data

--- index.js
+++ index.js
@@ -6,24 +6,20 @@ const credentials = require("./key.json");
 admin.initializeApp({
 credential:admin.credential.cert(credentials)
 });
-
 const db =admin.firestore();
 app.use(express.text())
  
-
 //--------------------------Variavel do ID ---------------------------------------------------
 let idesp = "";
  
 //--------------------------------------------------------------------------------------------
 
 //---------------Dados time e data-------------------------------------------------------------
-let data = new Date();
-let diames = data.getDate().toString();
-let mesesp = (data.getMonth()+1).toString() + + data.getFullYear().toString()
-let diasemana = data.getDay() ;
 
-console.log(mesesp)
+
+
 //---------------------------------------------------------------------------------------------
+
 
 
  //---------Função Calcula hora da semana e hora do mês-------------------------------------------
@@ -31,36 +27,47 @@ console.log(mesesp)
 
   let sumhs = 0;
   let sumhm = 0;
-
-  for(i=diasemana;i>=0; i--){
-
-    let auxdmes = diames - i;
+  let data = new Date();
+  let diames = data.getDate().toString();
+  let mesesp = "Data"
+  let anomes = (data.getMonth()+1).toString()+( data.getFullYear().toString())
+  let diasemana = data.getDay() ;
   
+  for(i=(diasemana);i>=0; i--){
+
+    let auxdmes = (anomes+diames) - i;
+    
+    
     await db.collection('users').doc(idesp).collection(mesesp).doc(auxdmes.toString()).get().then(function(doc){
 
       const dadosaux =doc.data();
-      
+      console.log(auxdmes + "aqui é o auxiliar")
       if(doc.exists){
 
         sumhs = dadosaux.hora_dia + sumhs;
         console.log(auxdmes + "primeiro for");
+        
 
       }
 
     })
-    //console.log(i);
+    
   }
 
   for(i=diames;i>=0; i--){
 
-    let auxdmes = diames - i;
-  
+    let auxdmes = anomes+diames - i;
+    
      
     await db.collection('users').doc(idesp).collection(mesesp).doc(auxdmes.toString()).get().then(function(doc){
 
       const dadosaux =doc.data();
       
       if(doc.exists){
+
+      db.collection('users').doc(idesp).collection(mesesp).doc(auxdmes.toString()).update({  //update ultimodia
+          ultimodia:false  
+        })  
 
         sumhm = dadosaux.hora_dia + sumhm;
         console.log(auxdmes);
@@ -70,9 +77,10 @@ console.log(mesesp)
     
   }
   
-  db.collection('users').doc(idesp).collection(mesesp).doc(diames).update({  //update dos dados no banco de dados
+  await db.collection('users').doc(idesp).collection(mesesp).doc(anomes+diames).update({  //update dos dados no banco de dados
     hora_semana : sumhs,
     hora_mes: sumhm,
+    ultimodia:true
     
   })  
 }
@@ -82,7 +90,13 @@ console.log(mesesp)
 app.post('/data',async (req,res)=>{         //Caminho do Post do HTTP
 
  let idbody = req.body;
- console.log(idbody);
+
+ let data = new Date();
+ let diames = data.getDate().toString();
+ let mesesp = "Data"
+ let anomes = (data.getMonth()+1).toString()+( data.getFullYear().toString())
+ let diasemana = data.getDay() ;
+
  const usersRef = db.collection('users');
  const queryRef = await usersRef.where('idcard', '==', idbody).get(); // Encontra se o ID esta registrado
 
@@ -102,30 +116,33 @@ queryRef.forEach(doc => {          // se achar o ID , atribuiu o id uid do authe
 
 ///* -----------SE o ID EXISTE------------------------------------------------------------------------------------------------
 db.collection('users').doc(idesp).get().then(async function(doc){
-  if(doc.exists){ 
+
+  let dadosUsers = doc.data();
+
+  if(doc.exists && dadosUsers.hora_chegada !==0){ 
 
     console.log("existe o id " + idesp);
     
-    let dadosUsers = doc.data();
+    
     
 //----verifica se existe a coleção do dia pro primeiro registro, se existir atualiza o segundo registro
-   await db.collection('users').doc(idesp).collection(mesesp).doc(diames).get().then( async function(doc){
+   await db.collection('users').doc(idesp).collection(mesesp).doc(anomes+diames).get().then( async function(doc){
 
     let dadosdia = doc.data();   // Dados do dia do Usuario.
 
-    if(doc.exists  ){   // se o documento existir , vai atualizar o segundo horario e chamar a funcão de calculo de horas
+    if(doc.exists && dadosdia.hora_chegada !== 0 ){   // se o documento existir , vai atualizar o segundo horario e chamar a funcão de calculo de horas
       
-      console.log("existe a pasta  e o ano" + diames);
+      console.log("existe a pasta  e o ano" + anomes+diames);
       
       
-      await db.collection('users').doc(idesp).collection(mesesp).doc(diames).update({  //update dos dados no banco de dados
+      await db.collection('users').doc(idesp).collection(mesesp).doc(anomes+diames).update({  //update dos dados no banco de dados
         hora_saida: new Date(),
         hora_dia:  Date.now() - dadosdia.hora_chegada.toDate()  //subtrai a hora de chegada e a de saida e guarda na variavel hora_dia
       })  
       
       calc_hshm();  // função para calcular hora da semana, hora do mês;
       
-      console.log("update na pasta " + diames);
+      console.log("update na pasta " + anomes+diames);
 
       res.end("BYE: " + dadosUsers.nome);  // manda a msg pro esp32 "BYE: "Nome do usuario" " 
       
@@ -133,7 +150,7 @@ db.collection('users').doc(idesp).get().then(async function(doc){
 
     else{      // SE NÃO EXISTIR A COLEÇÃO , CRIA  E COLOCA O PRIMEIRO HORARIO , Junto com os dados
 
-      console.log("Não existe a pasta " + diames);
+      console.log("Não existe a pasta " + anomes+diames);
 
 //-------------------------Dados para o Banco de Dados----------------------------------------
       const dataesp = {
@@ -146,12 +163,14 @@ db.collection('users').doc(idesp).get().then(async function(doc){
       anopasta: data.getFullYear(),
       mespasta:(data.getMonth()+1),
       diamespasta:data.getDate(),
-      userid:idesp,
+      ultimodia: true,
       nomec:dadosUsers.nome+" "+dadosUsers.sobrenome,
-      meta:dadosUsers.meta
+      meta:dadosUsers.meta,
+      cpf:dadosUsers.cpf,
+      equipe:dadosUsers.equipe
     };
 
-      await db.collection('users').doc(idesp).collection(mesesp).doc(diames).set(dataesp);
+      await db.collection('users').doc(idesp).collection(mesesp).doc(anomes+diames).set(dataesp);
       calc_hshm();  // função para calcular hora da semana, hora do mês;
       console.log(idesp +"criando a pasta")
 
@@ -162,9 +181,7 @@ db.collection('users').doc(idesp).get().then(async function(doc){
   }
 
 });
-
 //------------------------------------------------------------------------------------------
-
 })
 
 app.listen (8080,()=>{
